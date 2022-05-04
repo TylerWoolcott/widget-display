fa69528eb86aba45c35e37101edd0078a745f59a
Tyler Woolcott <Tyler.woolcott@gmail.com>
2022-05-04 17:19:40 +0100

    update readme

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="container">
       <div className="widget">
-          <intelligence-widget apiKey="YOUR_API_KEY_HERE" topicId="a1Gb0000000LGk6EAG"></intelligence-widget>          
+          <intelligence-widget apiKey="YOUR_API_KEY_HERE" topicId="a1Gb0000000LGk6EAG" language="en"></intelligence-widget>          
       </div>
       <Button variant="primary" onClick={() => setModalShow(true)}>
         Embed Widget
@@ -19,7 +19,7 @@ function App() {
         show={modalShow}
         onHide={() => setModalShow(false)}
       />
-    </div>
+      </div>
   );
 }
 
